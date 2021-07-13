de575455a7839480f4b1ba3fec199c5347c3917f
R0m4in-dooz <72380352+R0m4in-dooz@users.noreply.github.com>
2021-07-13 16:09:52 +0200

    Feature: misc. changes (#44)
    * remove hex_utils.dart
    
    * change docs on library file
    
    * added export statements
    
    * added check String is only spaces for isBlank extension

--- lib/dooz_gateway_sdk.dart
+++ lib/dooz_gateway_sdk.dart
@@ -1,6 +1,7 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// A Dart SDK to interact with DooZ ooPLA product (IP-BT gateway) and enable control over DooZ mesh n/w.
 library dooz_gateway_sdk;
 
+export 'src/constants.dart' show LogLevel;
 export 'src/dooz_gateway.dart';
+export 'src/exceptions/errors.dart';
+export 'src/models/models.dart';
--- lib/src/utils/hex_utils.dart
+++
@@ -1,9 +0,0 @@
-List<int> intToList(int value) {
-  final byteArray = [0, 0, 0, 0];
-  for (var index = 0; index < byteArray.length; index++) {
-    final byte = value & 0xff;
-    byteArray[byteArray.length - 1 - index] = byte;
-    value = value >> 8;
-  }
-  return byteArray;
-}
