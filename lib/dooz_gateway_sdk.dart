0e01397cd1adbf767cf33b99c6059ab62fac1be6
R0m4in-dooz <romain@dooz-domotique.com>
2021-07-13 16:01:46 +0200

    change docs on library file

--- lib/dooz_gateway_sdk.dart
+++ lib/dooz_gateway_sdk.dart
@@ -1,6 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// A Dart SDK to interact with DooZ ooPLA product (IP-BT gateway) and enable control over DooZ mesh n/w.
 library dooz_gateway_sdk;
 
 export 'src/dooz_gateway.dart';
