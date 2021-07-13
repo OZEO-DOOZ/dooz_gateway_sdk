6a9e85dbc2e1272ed177db5865d850338e1a3e73
R0m4in-dooz <romain@dooz-domotique.com>
2021-07-13 16:03:24 +0200

    added export statements

--- lib/dooz_gateway_sdk.dart
+++ lib/dooz_gateway_sdk.dart
@@ -1,4 +1,7 @@
 /// A Dart SDK to interact with DooZ ooPLA product (IP-BT gateway) and enable control over DooZ mesh n/w.
 library dooz_gateway_sdk;
 
+export 'src/constants.dart' show LogLevel;
 export 'src/dooz_gateway.dart';
+export 'src/exceptions/errors.dart';
+export 'src/models/models.dart';
